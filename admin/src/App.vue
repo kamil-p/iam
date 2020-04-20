71ae9e45b30a34bda952dd9a03a975b1efc63440
Kamil Purzynski <kamil.purzynski@gmail.com>
2020-04-20 20:59:32 +0200

    Created login page

---
+++ admin/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <Login/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Login from './components/Login';
+
+export default {
+  name: 'App',
+
+  components: {
+    Login,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
