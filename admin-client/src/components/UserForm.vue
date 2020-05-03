2c6e4ca80ad7487099821013b28ef707e011744e
Kamil Purzynski <kamil.purzynski@gmail.com>
2020-05-03 21:32:14 +0200

    Added vee-validate, and form for editing user (not finished yet)

--- admin-client/src/components/UserForm.vue
+++ admin-client/src/components/UserForm.vue
@@ -1,34 +1,73 @@
 <template>
-    <h1>kamil</h1>
+    <ValidationObserver ref="observer" >
+        <form>
+            <ValidationProvider v-slot="{ errors }" name="Name" rules="required|max:10">
+                <v-text-field
+                        v-model="name"
+                        :counter="10"
+                        :error-messages="errors"
+                        label="Name"
+                        required
+                ></v-text-field>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ errors }" name="email" rules="required|email">
+                <v-text-field
+                        v-model="email"
+                        :error-messages="errors"
+                        label="E-mail"
+                        required
+                ></v-text-field>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ errors }" name="select" rules="required">
+                <v-select
+                        v-model="select"
+                        :items="items"
+                        :error-messages="errors"
+                        label="Select"
+                        data-vv-name="select"
+                        required
+                ></v-select>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ errors }" rules="required" name="checkbox">
+                <v-checkbox
+                        v-model="checkbox"
+                        :error-messages="errors"
+                        value="1"
+                        label="Option"
+                        type="checkbox"
+                        required
+                ></v-checkbox>
+            </ValidationProvider>
+
+            <v-btn class="mr-4" @click="submit">submit</v-btn>
+            <v-btn @click="clear">clear</v-btn>
+        </form>
+    </ValidationObserver>
 </template>
 
 <script>
-    //import { required, email, max } from 'vee-validate/dist/rules'
-    //import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
-
-    //setInteractionMode('eager')
-
-    /*extend('required', {
+    import { required, email, max } from 'vee-validate/dist/rules'
+    import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
+    setInteractionMode('eager')
+    extend('required', {
         ...required,
         message: '{_field_} can not be empty',
     })
-
     extend('max', {
         ...max,
         message: '{_field_} may not be greater than {length} characters',
     })
-
     extend('email', {
         ...email,
         message: 'Email must be valid',
-    })*/
+    })
 
     export default {
         name: 'UserForm',
-        // components: {
-        //     ValidationProvider,
-        //     ValidationObserver,
-        // },
+        components: {
+            ValidationProvider,
+            ValidationObserver,
+        },
         data: () => ({
             name: '',
             email: '',
@@ -41,7 +80,6 @@
             ],
             checkbox: null,
         }),
-
         methods: {
             submit () {
                 this.$refs.observer.validate()
